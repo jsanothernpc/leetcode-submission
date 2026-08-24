97d238c1d700c1a08645f564a4accbdf0891c2d7
Ayushman Pathak <pathakayush1309@gmail.com>
2026-08-24 19:05:47 +0530

    Time: 3 ms (15.63%), Space: 9.3 MB (67.77%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int freq[128] = {0};
+        for(char ch : s){
+            freq[ch]++;
+        }
+        int ans = 0;
+        int hasOdd = false;
+        for(int i=0;i<128;i++){
+            if(freq[i] % 2 == 0) ans+= freq[i];
+            else{
+                ans+=freq[i]-1;
+                hasOdd = true;
+            }   
+        }
+        
+        if(hasOdd) ans++;
+        return ans;
+    }
+};
